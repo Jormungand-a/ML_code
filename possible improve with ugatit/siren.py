2d594a8f65fa55efd539727e9bbfe00a42736566
Jormungand-a <43802704+Jormungand-a@users.noreply.github.com>
2020-07-04 20:46:02 +0800

    Add files via upload

---
+++ possible improve with ugatit/siren.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 15:17:04 2020
+@author: admin
+"""
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+import torch.nn.functional as F
+
+
+class SCConv(nn.Module):
+    def __init__(self, inplanes, planes, stride, padding, dilation, groups, pooling_r, norm_layer):
+        super(SCConv, self).__init__()
+        self.k2 = nn.Sequential(
+                    nn.AvgPool2d(kernel_size=pooling_r, stride=pooling_r), 
+                    nn.Conv2d(inplanes, planes, kernel_size=3, stride=1,
+                                padding=padding, dilation=dilation,
+                                groups=groups, bias=False),
+                    norm_layer(planes),
+                    )
+        self.k3 = nn.Sequential(
+                    nn.Conv2d(inplanes, planes, kernel_size=3, stride=1,
+                                padding=padding, dilation=dilation,
+                                groups=groups, bias=False),
+                    norm_layer(planes),
+                    )
+        self.k4 = nn.Sequential(
+                    nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+                                padding=padding, dilation=dilation,
+                                groups=groups, bias=False),
+                    norm_layer(planes),
+                    )
+
+    def forward(self, x):
+        identity = x
+
+        out = torch.sigmoid(torch.add(identity, F.interpolate(self.k2(x), identity.size()[2:]))) # sigmoid(identity + k2)
+        out = torch.mul(self.k3(x), out) # k3 * sigmoid(identity + k2)
+        out = self.k4(out) # k4
+
+        return out
+
+
+
+class SCBottleneck(nn.Module):
+    """SCNet SCBottleneck
+    """
+    expansion = 1
+    pooling_r = 2 # down-sampling rate of the avg pooling layer in the K3 path of SC-Conv.
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None,
+                 cardinality=1, bottleneck_width=128,
+                 avd=False, dilation=1, is_first=False,
+                 norm_layer=nn.InstanceNorm2d):
+        super(SCBottleneck, self).__init__()
+        group_width = 128
+        self.conv1_a = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+        self.in1_a = norm_layer(group_width)
+        self.conv1_b = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+        self.in1_b = norm_layer(group_width)
+        self.avd = avd and (stride > 1 or is_first)
+
+        if self.avd:
+            self.avd_layer = nn.AvgPool2d(3, stride, padding=1)
+            stride = 1
+
+        self.k1 = nn.Sequential(
+                    nn.Conv2d(
+                        group_width, group_width, kernel_size=3, stride=stride,
+                        padding=dilation, dilation=dilation,
+                        groups=cardinality, bias=False),
+                    norm_layer(group_width),
+                    )
+
+        self.scconv = SCConv(
+            group_width, group_width, stride=stride,
+            padding=dilation, dilation=dilation,
+            groups=cardinality, pooling_r=self.pooling_r, norm_layer=norm_layer)
+
+        self.conv3 = nn.Conv2d(
+            group_width * 2, planes * 1, kernel_size=1, bias=False)
+        self.bn3 = norm_layer(planes*1)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.dilation = dilation
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out_a= self.conv1_a(x)
+        out_a = self.in1_a(out_a)
+        out_b = self.conv1_b(x)
+        out_b = self.in1_b(out_b)
+        out_a = self.relu(out_a)
+        out_b = self.relu(out_b)
+
+        out_a = self.k1(out_a)
+        out_b = self.scconv(out_b)
+        out_a = self.relu(out_a)
+        out_b = self.relu(out_b)
+
+        if self.avd:
+            out_a = self.avd_layer(out_a)
+            out_b = self.avd_layer(out_b)
+
+        out = self.conv3(torch.cat([out_a, out_b], dim=1))
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+
+
+
+class scada(nn.Module):
+    """SCNet SCBottleneck
+    """
+    expansion = 1
+    pooling_r = 2 # down-sampling rate of the avg pooling layer in the K3 path of SC-Conv.
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None,
+                 cardinality=1, bottleneck_width=128,
+                 avd=False, dilation=1, is_first=False,
+                 norm_layer=nn.InstanceNorm2d):
+        super(scada, self).__init__()
+        group_width = 128
+        self.conv1_a = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+        self.in1_a = norm_layer(group_width)
+        self.conv1_b = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+        self.in1_b = norm_layer(group_width)
+        self.avd = avd and (stride > 1 or is_first)
+
+        if self.avd:
+            self.avd_layer = nn.AvgPool2d(3, stride, padding=1)
+            stride = 1
+
+        self.k1 = nn.Sequential(
+                    nn.Conv2d(
+                        group_width, group_width, kernel_size=3, stride=stride,
+                        padding=dilation, dilation=dilation,
+                        groups=cardinality, bias=False),
+                    norm_layer(group_width),
+                    )
+
+        self.scconv = SCConv(
+            group_width, group_width, stride=stride,
+            padding=dilation, dilation=dilation,
+            groups=cardinality, pooling_r=self.pooling_r, norm_layer=norm_layer)
+
+        self.conv3 = nn.Conv2d(
+            group_width * 2, planes * 1, kernel_size=1, bias=False)
+        #self.bn3 = norm_layer(planes*1)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.dilation = dilation
+        self.stride = stride
+        self.norm2 = adaILN(planes)
+    def forward(self, x,gamma,beta):
+        residual = x
+
+        out_a= self.conv1_a(x)
+        out_a = self.in1_a(out_a)
+        out_b = self.conv1_b(x)
+        out_b = self.in1_b(out_b)
+        out_a = self.relu(out_a)
+        out_b = self.relu(out_b)
+
+        out_a = self.k1(out_a)
+        out_b = self.scconv(out_b)
+        out_a = self.relu(out_a)
+        out_b = self.relu(out_b)
+
+        if self.avd:
+            out_a = self.avd_layer(out_a)
+            out_b = self.avd_layer(out_b)
+
+        out = self.conv3(torch.cat([out_a, out_b], dim=1))
+        out = self.norm2(out, gamma, beta)
+
+        #if self.downsample is not None:
+        #    residual = self.downsample(x)
+
+        out += residual
+        return out
+
+
+class adaILN(nn.Module):
+    def __init__(self, num_features, eps=1e-5):
+        super(adaILN, self).__init__()
+        self.eps = eps
+        self.rho = Parameter(torch.Tensor(1, num_features, 1, 1))
+        self.rho.data.fill_(0.9)
+
+    def forward(self, input, gamma, beta):
+        in_mean, in_var = torch.mean(input, dim=[2, 3], keepdim=True), torch.var(input, dim=[2, 3], keepdim=True)
+        out_in = (input - in_mean) / torch.sqrt(in_var + self.eps)
+        ln_mean, ln_var = torch.mean(input, dim=[1, 2, 3], keepdim=True), torch.var(input, dim=[1, 2, 3], keepdim=True)
+        out_ln = (input - ln_mean) / torch.sqrt(ln_var + self.eps)
+        out = self.rho.expand(input.shape[0], -1, -1, -1) * out_in + (1-self.rho.expand(input.shape[0], -1, -1, -1)) * out_ln
+        out = out * gamma.unsqueeze(2).unsqueeze(3) + beta.unsqueeze(2).unsqueeze(3)
+
+        return out
+
+
+
+
+
+
+
+
